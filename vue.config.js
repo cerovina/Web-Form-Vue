19690638571292664febfdc4f716c8a2ddc6376c
Nikola Cerovina <cerovina1997@gmail.com>
2023-10-09 18:56:26 +0200

    Adding everything

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/Web-Form/' : '/',
-};
-
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
